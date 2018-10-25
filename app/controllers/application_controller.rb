f7b0972ff3510ea2e7e2bff2ed80611eec4e4eeb
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-10-25 16:27:22 -0700

    set sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "this is in no way secret enough and must be changed later"
   end
 
   get "/" do
