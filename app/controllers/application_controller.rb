4287a4d0ce2c749ce5cd8fd41520363c3a43c0c3
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-10-25 17:47:50 -0700

    create helper methods #current_user and #logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+    
+    def current_user
+      @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in
+      !!current_user
+    end
+
+  end
+
 end
