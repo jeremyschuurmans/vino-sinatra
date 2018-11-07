21da21ffa1f556971ebdc24d2e12f3116fbc8819
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-11-07 12:21:54 -0800

    enable user's browser to render wines index if user still logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    if logged_in
+      redirect '/wines'
+    else
+      erb :index
+    end
   end
 
   helpers do
