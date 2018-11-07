419cd147e7c3b5fc618869f2201aee7dc088eee9
jeremy schuurmans <jeremy.shurminz@gmail.com>
2018-11-07 12:23:00 -0800

    debug edit wine function (does not edit correct wine), fix position of cards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,8 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
+    @user = current_user
+    @wine = Wine.find { |wine| wine.user_id == @user.id }
 
     erb :'/users/show'
   end
--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -42,21 +42,22 @@ class WinesController < ApplicationController
     end
   end
 
-  # get '/wines/:id/edit' do
-  #   if logged_in
-  #     @user = current_user
-  #     @wine = Wine.find_by(id: params[:id])
-  #     if @wine.user_id == current_user.id
-  #       erb :'wines/edit'
-  #     end
-  #   else
-  #     redirect '/login'
-  #   end
-  # end
+  get '/wines/:id/edit' do
+    if logged_in
+      @user = current_user
+      @wine = Wine.find_by(id: params[:id])
+      if @wine.user_id == current_user.id
+        erb :'wines/edit'
+      end
+    else
+      redirect '/login'
+    end
+  end
 
   patch '/wines/:id' do
     if logged_in
-      @wine = Wine.find_by(id: params[:id])
+      @user = current_user
+      @wine = Wine.find { |wine| wine.user_id == @user.id }
       if !params[:name].empty? && !params[:winery].empty? && !params[:vintage].empty? && !params[:origin].empty? && !params[:price].empty? && !params[:rating].empty? && !params[:tasting_notes].empty? && @wine.user_id == current_user.id
         @wine.update(name: params[:name], winery: params[:winery], vintage: params[:vintage], origin: params[:origin], price: params[:price], rating: params[:rating], tasting_notes: params[:tasting_notes], other_notes: params[:other_notes])
         redirect "/wines/#{@wine.id}"
